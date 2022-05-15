b9e5b44c773cf8662532ebde94c03567ee26cbc1
Shibzukhov Astemir <a.shibzukhov@tinkoff.ru>
2022-05-15 21:43:53 +0300

    starting settings

---
+++ SAHLSPlayer/SAHLSPlayer.h
@@ -0,0 +1,18 @@
+//
+//  SAHLSPlayer.h
+//  SAHLSPlayer
+//
+//  Created by Shibzukhov Astemir on 15.05.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SAHLSPlayer.
+FOUNDATION_EXPORT double SAHLSPlayerVersionNumber;
+
+//! Project version string for SAHLSPlayer.
+FOUNDATION_EXPORT const unsigned char SAHLSPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SAHLSPlayer/PublicHeader.h>
+
+
